8e940248d6d6476ff038ab38315e888b853c9aa7
beef <ubeef@hotmail.com>
2018-05-19 18:10:50 +0800

    完成小车启动

--- src/src.ino
+++ src/src.ino
@@ -3,16 +3,19 @@
 //小车端口
 int port[] = {5, 6, 10, 11};
 //前后左右
-int forward[] = {255, 0, 255, 0};
-int back[] = {0, 255, 0, 255};
-int left[] = {0, 255, 255, 0};
-int right[] = {255, 0, 0, 255};
-
+//  建议不要设置整体值太大
+int forward[] = {128, 0, 128, 0};
+int back[] = {0, 128, 0, 128};
+int left[] = {0, 128, 128, 0};
+int right[] = {128, 0, 0, 128};
+int stack[] = {0, 0, 0, 0};
 void setup() {
     portSetup(port);
 }
 
 void loop() {
   control(port, 5000, forward);
+  control(port, 500, stack);
   control(port, 2000, left);
+  control(port, 500, stack);  
 }
