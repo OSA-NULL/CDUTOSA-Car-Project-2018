4580b8bc79d7ea5aafc4f8afc526720df883c354
Dawn.L <lzqq64@qq.com>
2018-05-20 17:44:51 +0800

    修复 motor_ctl 代码及格式问题

--- src/motor_ctl.c
+++ src/motor_ctl.c
@@ -1,9 +1,9 @@
 #include "motor_ctl.h"
 
 void
-motorControl(int* port, int times, int degree[])
+motorControl(int* port, int times, direction_t direction)
 {
-    int direction_arr[] = {
+    int direction_arr[][4] = {
         {128, 0, 128, 0},
         {0, 128, 0, 128},
         {0, 128, 128, 0},
@@ -11,9 +11,9 @@ motorControl(int* port, int times, int degree[])
         {0, 0, 0, 0}
     };
 
-for(int i = 0; i < 3；i++){ 
-    analogWrite(port[i], degree[3]);
-}
+    for(int i = 0; i < 3; i++){ 
+        analogWrite(port[i], direction_arr[direction][i]);
+    }
     delay(times);
 }
 
--- src/motor_ctl.h
+++ src/motor_ctl.h
@@ -1,20 +1,24 @@
 #ifndef __MOTOR_CTL_H__
 #define __MOTOR_CTL_H__
 
- /*
-  * void motorControl(int* port, int times, int* degree)
-  * 功能：
-  *     控制小车动作
-  * 参数：
-  *     port:小车控制端口数组 为[右前，右后，左前，左后]
-  *     times:动作持续时间
-  *     degree：控制幅度(PWM值)(0~255)
-  * 返回：
-  *     无
-  */
-typedef enum {
+#include <Arduino.h>
+
+enum Directions{
     FORWARD, BACK, LEFT, RIGHT, PARK
-} direction_t;
+};
+typedef enum Directions direction_t;
+
+/*
+ * void motorControl(int* port, int times, int* degree)
+ * 功能：
+ *     控制小车动作
+ * 参数：
+ *     port:小车控制端口数组 为[右前，右后，左前，左后]
+ *     times:动作持续时间
+ *     degree：控制幅度(PWM值)(0~255)
+ * 返回：
+ *     无
+ */
 void
 motorControl(int* port, int times, direction_t direction);
 
@@ -30,10 +34,10 @@ motorControl(int* port, int times, direction_t direction);
 inline void 
 motorSetup(int* port)
 {
-int i = 0;
-for(; i < 4; i++){
-    pinMode(port[i], INPUT);
-}
+    int i = 0;
+    for(; i < 4; i++){
+        pinMode(port[i], INPUT);
+    }
 }
 /*
  * void forward
