b6a13931db011fddc0e2a971bbc286f33a37b8cd
beef <ubeef@hotmail.com>
2018-05-28 16:49:52 +0800

    0528,加装光敏电阻 改写驱动 并用interface.md描述接口“

--- src/src.ino
+++ src/src.ino
@@ -5,15 +5,20 @@
 int motorPort[] = {5, 6, 10, 11};
 //小车测距端口 [Trig, Echo]
 int ultraPort[] = {2,3};
+//小车测光端口 [输入]
+int photoSensitive = A0;
 
 void setup() {
     motorSetup(motorPort);  //初始化马达端口
     ultraSetup(ultraPort);  //初始化超声波端口
+    pinMode(photoSensitive, INPUT);
 }
 
 void loop() {
     if(distance(ultraPort) >= 25)
         motorControl(motorPort, 300, FORWARD);
     else
+        motorControl(motorPort, 200, BACK); 
+    if(photoSensitive < 300)
         motorControl(motorPort, 200, BACK); 
 }
