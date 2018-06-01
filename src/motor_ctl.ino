bd8ea4476cf1d657cc88ed4a813130bfdf52dbd1
beef <ubeef@hotmail.com>
2018-06-01 20:03:09 +0800

    修复左轮不转的问题

--- src/motor_ctl.ino
+++ src/motor_ctl.ino
@@ -4,14 +4,14 @@ void
 motorControl(int* port, int times, direction_t direction)
 {
     int direction_arr[][4] = {
-        {128, 0, 128, 0},
-        {0, 128, 0, 128},
-        {0, 128, 128, 0},
-        {128, 0, 0, 128},
-        {0, 0, 0, 0},
+        {128,   0, 128,   0},
+        {  0, 128,   0, 128},
+        {  0, 128, 128,   0},
+        {128,   0,   0, 128},
+        {  0,   0,   0,   0},
     };
 
-    for(int i = 0; i < 3; i++){ 
+    for(int i = 0; i <= 3; i++){ 
         analogWrite(port[i], direction_arr[direction][i]);
     }
     delay(times);
--- src/src.ino
+++ src/src.ino
@@ -1,10 +1,12 @@
 #include "motor_ctl.h"
+#include "stdio.h"
 #include "sensor.h"
+void Back(void);
 
 //小车马达端口 [右前，右后，左前，左后]
-int motorPort[] = {5, 6, 10, 11};
+int motorPort[] = {10, 11, 5, 6};
 //小车测距端口 [Trig, Echo]
-int ultraPort[] = {2,3};
+int ultraPort[] = {2,A1};
 //小车测光端口 [输入]
 int senPort = A0;
 
@@ -18,6 +20,10 @@ void loop() {
     if(distance(ultraPort) >= 25)
         if(senLight(senPort) < 300)
             motorControl(motorPort, 200, FORWARD);
+        else
+            motorControl(motorPort, 200, BACK);
     else
-        motorControl(motorPort, 200, BACK);
+            motorControl(motorPort, 200, BACK);
 }
+
+
